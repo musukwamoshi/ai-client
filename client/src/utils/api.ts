44b8e91904317267761e0ecb095fbfdcae637b97
Moshi <29588163+musukwamoshi@users.noreply.github.com>
2024-02-03 17:38:55 +0200

    added debug logs to api util

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -31,6 +31,8 @@ const run = async (path: any, method: any, headers?: any, body?: any) => {
         body, credentials: 'include',
         headers, method,
     });
+    console.log('response from fetch');
+    console.log(response);
     const { ok } = response;
     let result;
     try {
